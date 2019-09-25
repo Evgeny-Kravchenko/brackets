15500ffed67de447065177e6c28b3fa9205ea43c
Evgeny Kravchenko <evgen.kravchenko.vladimirovich@mail.ru>
2019-09-25 19:08:03 +0300

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for(let i = 0; i < 100; i++) {                                        //Проходим опредеоённое колличество раз по str, чтобы проверить все возможные вложенности
+        for(let j = 0; j < bracketsConfig.length; j++) {                    //Проходи каждой парой скобок по str
+            let bracketsConfigStr = bracketsConfig[j].join("");             //Преобразовываем bracketsConfig в строку
+            while(str.includes(bracketsConfigStr)) {                        //Удаляем через цикл все совпадения из str после чего переходим, переходим на новую итерацию для проверки brackets другого типа
+                str = str.replace(bracketsConfigStr, "");                   /*После проверки всех скобок переходим на новую итерацию внешенего цикла for чтобы родительские brackets                      */
+            }
+        }                                 //Если удалено не всё, значит скобки расставлены не правильно
+    }
+    if(str.length === 0) return true;                                     //Если удалено всё, то скобки расставлены правильно
+    if(str.length !== 0) return false;   
 }
